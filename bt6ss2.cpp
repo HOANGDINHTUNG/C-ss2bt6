73fdb1a78e02d025f51b35d8108e22846d53049f
HOANGDINHTUNG <mtung3365@gmail.com>
2024-12-07 21:02:41 +0700

    Add files via upload

---
+++ bt6ss2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	float pi = 3.14;
+	int bk = 7;
+	double chu_vi = 2 * pi * bk;
+	double dien_tich = pi * bk * bk;
+	printf("chu_vi = %.2lf\ndien_tich = %.2lf", chu_vi, dien_tich);
+
+}
